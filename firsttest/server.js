3edc6bd3aac08c7ab5801984ddc47562c025ffd0
Razorbob <vincent.dibon@gmail.com>
2016-04-23 15:21:05 +0200

    Express and Routes

--- firsttest/server.js
+++ firsttest/server.js
@@ -1,1 +1,40 @@
-console.log("HELLO WORLD MOFO!");
+var express = require('express'),
+	app = express(),
+	path = require('path'),
+	adminRouter = express.Router();
+
+
+adminRouter.use(function(req,res,next){
+	console.log(req.method, req.url);
+	next();
+});
+
+// localhost:1337/admin/
+adminRouter.get('/', function(req,res){
+	res.send('Dashboard');
+});
+// localhost:1337/admin/users
+adminRouter.get('/users', function(req,res){
+	res.send('Users');
+});
+// localhost:1337/admin/posts
+adminRouter.get('/posts', function(req,res){
+	res.send('Posts');
+});
+
+app.use('/admin',adminRouter);
+
+/*
+ *  Send Index to User Requesting localhost:1337/
+ *	example:   www.johannes.com
+ */
+ app.get('/',function(req,res){
+ 	res.sendFile(path.join(__dirname+'/index.html'));
+ });
+
+
+/*
+ *  Let the APP listen to Port 1337
+ */
+ app.listen(1337);
+console.log('Server startet Localhost:1337');
